37b604c44a91b07aed1ae05c08419ba9619aeb41
wj5403293 <168689837+wj5403293@users.noreply.github.com>
2024-07-10 09:27:25 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 mkdir -p build
 cmake -G Ninja -S . -B build \
-      -DCMAKE_C_COMPILER="clang" \
-      -DCMAKE_CXX_COMPILER="clang" \
       -DCMAKE_INSTALL_PREFIX="install" \
       -DCMAKE_BUILD_TYPE=Release
 ninja -j`nproc` -C build install
